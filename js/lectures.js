bc60d66809a784bc4efe1375223ac122ce68fe80
Yurij Husnaj <husnaj23@gmail.com>
2012-06-14 13:33:47 +0300

    second-commit

---
+++ js/lectures.js
@@ -0,0 +1,9 @@
+var Lecture = function (param) {
+	this.English = {
+		level1 : {
+			newLetters : ['a', 'b'],
+			lesson : 'ababab ba bab ab ba bab ab ba b'
+		}
+	}// end English
+	
+}// end Lecture
---
+++ js/stats.js
@@ -0,0 +1,26 @@
+var Statistic = function () {
+	this.startPoint = 0;
+	this.endPoint = 0;
+
+	this.timeElapsed = 0;
+	this.symbolsTyped = 0;
+	this.wordsTypes = 0;
+	this.mistakesMade = 0;
+	
+	this.speedSymbols = 0;
+	this.speedWords = 0;
+	this.Accuracy = 0;
+
+	this.setSpeedSymbols = function () {
+		
+	}
+	this.setSpeedWords = function () {
+
+	}
+	this.setAccuracy = function () {
+
+	}
+}// end Statistic constructor
+
+// test
+var Stats = new Statistic();
